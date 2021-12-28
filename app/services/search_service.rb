05dfda62974aacf8d4c8f45428e6b2afcff6f489
ragu <ragu@clickl.jp>
2021-12-28 21:47:08 +0900

    sudachi support

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -35,7 +35,7 @@ class SearchService < BaseService
   end
 
   def perform_statuses_search!
-    definition = parsed_query.apply(StatusesIndex.filter(term: { searchable_by: @account.id }))
+    definition = parsed_query.apply(StatusesIndex).order(id: :desc)
 
     if @options[:account_id].present?
       definition = definition.filter(term: { account_id: @options[:account_id] })
